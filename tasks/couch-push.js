793a0defa51c30b60928f9d3c5da81c72e8e7588
Johannes J. Schmidt <schmidt@netzmerk.com>
2013-06-03 17:28:18 +0200

    fix a problem with db creation

--- tasks/couch-push.js
+++ tasks/couch-push.js
@@ -11,27 +11,21 @@
 module.exports = function(grunt) {
   var request = require('request');
 
-  function createDatabase(req, url, done) {
-    req(url, { method: 'PUT' }, function(err, resp, data) {
+  function createDatabase(auth, url, done) {
+    request.put(url, { json: true, auth: auth }, function(err, resp, data) {
+      grunt.log.write('Creating database ' + url + '...');
       if (err) {
         grunt.log.error(err);
-        done(err);
-        return;
+      } else {
+        grunt.log.ok();
       }
 
-      var ok = resp.statusCode === 201;
-      grunt.log.write('Creating database ' + url + '...');
-      if (ok) {
-        grunt.log.ok();
-      } else {
-        grunt.log.error(resp.statusCode, data);
-      }
-      done(null);
+      done(err);
     });
   }
   
-  function pushDocs(req, url, doc, done) {
-    req(url + '/_bulk_docs', { body: doc }, function(err, resp, data) {
+  function pushDocs(auth, url, doc, done) {
+    request.post(url + '/_bulk_docs', { body: doc, json: true, auth: auth }, function(err, resp, data) {
       grunt.log.write('Pushing ' + url + '...');
 
       if (err) {
@@ -48,27 +42,17 @@ module.exports = function(grunt) {
       } else {
         grunt.log.error(resp.statusCode, data);
       }
-
-      done(null);
+      
+      done(ok ? null : data);
     });
   }
   
   function push(doc, url, auth, done) {
-    var defaults = {
-      json: true,
-      method: 'POST'
-    };
-
-    if (auth && auth.user && auth.pass) {
-      defaults.auth = auth;
-    }
-
     var keys = {
       keys: doc.docs.map(function(d) { return d._id; })
     };
 
-    var req = request.defaults(defaults);
-    req(url + '/_all_docs', { body: keys }, function(err, resp, data) {
+    request.post(url + '/_all_docs', { body: keys, json: true, auth: auth }, function(err, resp, data) {
       if (err) {
         grunt.log.error(err);
         done(err);
@@ -85,13 +69,13 @@ module.exports = function(grunt) {
             }
           }
         }
-        return pushDocs(req, url, doc, done);
+        return pushDocs(auth, url, doc, done);
       }
 
       if (resp.statusCode === 404) {
         grunt.log.ok('database does not exist');
-        return createDatabase(req, url, function(success) {
-          pushDocs(req, url, doc, done);
+        return createDatabase(auth, url, function(success) {
+          pushDocs(auth, url, doc, done);
         });
       }
 
@@ -100,33 +84,32 @@ module.exports = function(grunt) {
     });
   }
 
+  var async = require('async');
   grunt.registerMultiTask('couch-push', 'Push CouchDB design documents.', function() {
     var options = this.options();
 
     // Merge task-specific and/or target-specific options with command line options and these defaults.
-    var auth = {
-      user: grunt.option('user') || options.user,
-      pass: grunt.option('pass') || options.pass
-    };
+    var user = grunt.option('user') || options.user;
+    var pass = grunt.option('pass') || options.pass;
+    var auth = user && pass ? { user: user, pass: pass } : null;
 
     var done = this.async();
     var files = this.files;
-    grunt.util.async.map(files, function(file, done) {
-      grunt.util.async.map(file.src, function(src, next) {
-        push(grunt.file.readJSON(src), file.dest, auth, next);
-      }, done);
+
+    // FIXME: Initiating parallel requests results in
+    // EPIPE or ECONNRESET errors when the database does not exist.
+    async.eachSeries(files, function(file, next) {
+      async.each(file.src, function(src, nextSrc) {
+        push(grunt.file.readJSON(src), file.dest, auth, nextSrc);
+      }, next);
     }, function(err) {
       if (err) {
         grunt.log.error(err);
-        done(false);
-        return;
+        return done(false);
       }
             
       grunt.log.ok(files.length + ' ' + grunt.util.pluralize(files.length, 'database/databases') + ' deployed');
       done(true);
-    });
-
-    this.files.forEach(function(file, i, files) {
     });
   });
 };
